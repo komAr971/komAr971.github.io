e985ece23e7caf348c196100a1d6b285a8d15e31
komAr971 <korostel97@gmail.com>
2019-09-15 23:06:07 +0300

    Add files via upload

---
+++ templates/about.php
@@ -0,0 +1,1 @@
+<button type="button" class="btn btn-primary">Если это кнопка, то бутстрап подсосан</button>
---
+++ templates/main.php
@@ -0,0 +1,364 @@
+<main>
+  <div id="carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="/img/carousel/reception.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="/img/carousel/office.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="/img/carousel/restroom.jpg" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    </a>
+    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    </a>
+  </div>  
+
+    <section class="services-menu">
+      <h2>Услуги клиники</h2>
+      <div class="services-box main">
+        <div class="service">
+          <div class="service_logo service_discount"></div>
+          <span class="service_name">Акции и скидки</span>
+          <span class="service_description">Выгодные предложения</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_prikus"></div>
+          <span class="service_name">Исправление прикуса</span>
+          <span class="service_description">Брекеты Дэймон</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_komponiry"></div>
+          <span class="service_name">Компониры</span>
+          <span class="service_description">Красивые зубы за один день</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_protez"></div>
+          <span class="service_name">Протезирование</span>
+          <span class="service_description">Коронки, винеры,съемные протезы</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_det"></div>
+          <span class="service_name">Детская стоматология</span>
+          <span class="service_description">Лечение без сверления!</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_3d"></div>
+          <span class="service_name">3D-диагностика</span>
+          <span class="service_description">Томография челюсти</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_plazmo"></div>
+          <span class="service_name">Плазмолифтинг для десен</span>
+          <span class="service_description">Уникальная технология</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_narkoz"></div>
+          <span class="service_name">Лечение под наркозом</span>
+          <span class="service_description">Общая анестезия</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_implantant"></div>
+          <span class="service_name">Имплантация</span>
+          <span class="service_description">Вставить новый зуб просто</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_micro"></div>
+          <span class="service_name">Лечение зубов под микроскопом</span>
+          <span class="service_description">Вооруженному глазу виднее</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_delete"></div>
+          <span class="service_name">Удаление зубов</span>
+          <span class="service_description">Бережно и аккуратно</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_white"></div>
+          <span class="service_name">Отбеливание Beyond</span>
+          <span class="service_description">Аппарат холодного света</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_lasern"></div>
+          <span class="service_name">Лазерное отбеливание</span>
+          <span class="service_description">Отбеливание с помощью SironaLaser</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_le4enie"></div>
+          <span class="service_name">Лечение зубов</span>
+          <span class="service_description">Кариес, пульпит, периодонтит, (терапия)</span>
+        </div>
+        <div class="service">
+          <div class="service_logo service_clear"></div>
+          <span class="service_name">Чистка зубов</span>
+          <span class="service_description">Гигиеническая чистка зубов</span>
+        </div>
+      </div>
+    </section>
+
+    <section class="technology-container">
+      <h2>Технологии</h2>
+      <div class="technology main">
+        <div class="technology_item">
+          <img src="/img/technology_photo_1.jpg">
+          <h3>Лечение без сверления</h3>
+          <p>Суть данного метода лечения зубов в механической обработке при помощи специального аппарата, мощным потоком воздуха удаляющего пораженные кариесом ткани</p>
+          <span><a href="#">Узнать подробнее</a></span>
+        </div>
+        <div class="technology_item">
+          <img src="/img/technology_photo_2.jpg">
+          <h3>3D-томография</h3>
+          <p>Внедрение в практику 3D-томографа — это переход от плоскостного видения к объемному изображению любого участка зубочелюстной системы</p>
+          <span><a href="#">Узнать подробнее</a></span>
+        </div>
+        <div class="technology_item">
+          <img src="/img/technology_photo_3.jpg">
+          <h3>Стоматологический микроскоп</h3>
+          <p>Стоматологический микроскоп позволяет лечить зубы под увеличением до 40 раз. Одновременно он освещает рабочее поле так, что доктор видит мельчайшие детали и свет проникает в закрытые ранее участки, которые без микроскопа не были видны</p>
+          <span><a href="#">Узнать подробнее</a></span>
+        </div>
+      </div>
+    </section>
+
+    <section class="reviews_news container main">
+      <div class="row">
+        <div class="reviews col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+          <h2>Отзывы</h2>
+          <article class="review">
+            <img src="/img/review_1.jpg">
+            <time>12.07.2019</time>
+            <h3>ПАЦИЕНТКА "ЦЕНТРА СТОМАТОЛОГИИ"</h3>
+            <div class="review-text">
+              <p>С чувством абсолютного доверия к доктору Берсеневой Л.Н. лечимся больше 10 лет в клинике "Центр Стоматологии".</p>
+              <p>Услуги всегда высокого качества. Сервис на высоком уровне.</p>
+              <p>В клинике обслуживается вся моя семья: муж, дочь, внук. Надеемся на дальнейшее сотрудничество.</p>
+              <p>Желаем дальнейших успехов и процветания!</p>
+              <p>Благодарим за помощь и благожелательность персонала клиники!</p>
+            </div>  
+          </article>
+          <article class="review">
+            <img src="/img/review_2.jpg">
+            <time>11.07.2019</time>
+            <h3>КУЛАБУКОВА Н.Н.</h3>
+            <div class="review-text">
+              <p>Очень довольна оказанием услуг врача высшей категории Берсеневой Ларисы Николаевны. Врач мастер своего дела, очень качественно оказывает услуги, очень компетентен с пациентами. Очень благодарна Ларисе Николаевне за оказанное профессиональное лечение.</p>
+              <p>С уважением и искренней благодарностью, Кулабукова Н.Н.</p>
+            </div>
+          </article>
+          <span><a href="#">Еще отзывы</a></span>
+        </div>
+        <div class="news col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+          <h2>Новости</h2>
+          <div class="news-box">
+            <article class="new">
+              <img src="/img/new_1.jpg">
+              <time>29.12.2018</time>
+              <h3>График работы в новогодние праздники</h3>
+              <div class="new-text">
+                <p>Искренне поздравляем с наступающим Новым годом!</p>
+                <p>Желаем вам в грядущем 2019 году быть в окружении исключительно положительных и доброжелательных людей, переживать только приятные эмоции, дарить радость и улыбки окружающим!</p>
+              </div>
+            </article>
+            <article class="new">
+              <img src="/img/new_2.jpg">
+              <time>04.11.2018</time>
+              <h3>Россия отмечает день народного единства</h3>
+              <div class="new-text">
+                <p>Это праздник, призванный напомнить о том, что наш народ един независимо от вероисповедания его представителей, их происхождения или занимаемого в обществе места.</p>
+                <p>А что объединяет людей по всему миру? Улыбка! Это универсальное средство, чтобы продемонстрировать человеку любой национальности свое хорошее отношение и дружелюбие. Однако похвастаться красивыми, идеально ровными и естественно белыми здоровыми зубами может далеко не каждый человек.</p>
+              </div>
+            </article>
+            <article class="new">
+              <img src="/img/new_3.jpg">
+              <time>03.04.2018</time>
+              <h3>От всей души: стоматологическая клиника поздравляет всех с пасхой!</h3>
+              <div class="new-text">
+                <p>Наверное, один из самых ожидаемых праздников весны – это Пасха. Она не только объединяет всех верующих христиан, но и привносит в нашу жизнь частичку чуда и веры в лучшее.</p>
+                <p>Для нашего коллектива и клиники в целом предстоящий праздник так же важен. </p>
+              </div>
+            </article>
+            <article class="new">
+              <img src="/img/new_4.jpg">
+              <time>06.03.2018</time>
+              <h3>График работы в праздничные дни</h3>
+              <div class="new-text">
+                <p>Наши милые и прекрасные пациентки!</p>
+                <p>Мы поздравляем вас с 8 марта и желаем всегда быть на высоте — на высоте своего великолепия, вдохновения и очарования. Будьте всегда любимы, счастливы, неотразимы и хороши собой!</p>
+              </div>
+            </article>
+          </div>
+          <span><a href="#">Еще новости</a></span>
+        </div>
+      </div>
+    </section>
+
+    <section class="abstract-container">
+      <div class="abstract main container">
+        <div class="row">
+          <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+            <h2>Социальная стоматология белогорья</h2>
+          </div>
+          <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+            <p>Лечение в "Центре стоматологии" производится на современном импортном стоматологическом оборудовании, которым оснащена клиника. Это гарантирует быстрое и высокое качество лечения, и абсолютную его безболезненность. Мы используем такие методики, как лечение кариеса на начальном этапе без сверления, а также лечение зубов под общим и местным наркозом, при этом используются анестетики последнего поколения.</p>
+            <p>Частная стоматологическая клиника оснащена компьютеризированными комплексами, оборудованными мониторами и интраоральными камерами, позволяющими с максимальной точностью произвести лечение зубов, например, кариеса и пульпита, не только взрослым, но и детям.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section class="result main container">
+    <div class="row">
+      <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+        <h2>Результат работы</h2>
+        <p>Результат нашей работы складывается из нескольких важных составляющих:</p>
+        <ul>
+          <li>
+            <div>
+              Безопасность
+            </div>
+            <p>
+              Мы прогрессивная стоматологическая клиника. Новосибирск — большой город,
+              но наш Центр в нём один из немногих, где в стерилизационных используется
+              2 автоклава ведущих мировых производителей итальянской компании Euronda,
+              прибор для чистки и смазки наконечников Cavo Quatro Care. Это дает уверенность
+              в том, что Ваше лечение будет проведено чистыми стерильными, абсолюно не
+              подвергающими опасности Ваше здоровье инструментами.
+            </p>
+          </li>
+          <li>
+            <div>
+              Специалисты с многолетним опытом
+            </div>
+            <p>
+              В нашем штате: профессор, кандидаты медицинских наук, врачи Высшей и Первой категории.
+            </p>
+          </li>
+          <li>
+            <div>
+              Точность диагностики
+            </div>
+            <p>
+              С помощью самых современных технологий, таких как компьютерная томография,
+              микроскоп, мы можем приблизить точность диагноза к 100%
+            </p>
+          </li>
+          <li>
+            <div>
+              Оснащение клиники современным импортным оборудованием
+            </div>
+            <p>
+              Благодаря новейшей технике, услуги стоматологической клиники оказываются
+              профессионально и на высоком уровне
+            </p>
+          </li>
+          <li>
+            <div>
+              Первоклассный сервис
+            </div>
+            <p>
+              Созданы все условия для комфортного ожидания приема как для взрослых,
+              так и для детей: оборудованная детская комната, зона отдыха, где Вам
+              предложат чашечку кофе или чая, массажное кресло
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-xl-4 col-lg-4 col-md-4 d-none d-lg-block d-xl-block result_img">
+        <img src="/img/baba.jpg"> 
+      </div>
+    </div>
+  </section>
+
+  <section class="why_we main container">
+      <h2>Почему мы?</h2>
+      <div class="row justify-content-center">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+          <img class="why_we_img" src="/img/why_we_1.png">
+          <div class="why_we_article">Отсутствие очередей</div>
+          <div class="why_we_reason">Принимаем всех пациентов в точно назначенное время</div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+          <img class="why_we_img" src="/img/why_we_2.png">
+          <div class="why_we_article">Гарантированная безопасность</div>
+          <div class="why_we_reason">Наша клиника строго придерживается всех требований санитарных норм.В работе
+            используются одноразовые расходные материалы и 100% стерильный инструментарий</div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+          <img class="why_we_img" src="/img/why_we_3.png">
+          <div class="why_we_article">Отвечаем за свою работу</div>
+          <div class="why_we_reason">Предоставляем гарантию на все виды услуг</div>
+        </div>
+      </div>
+    </section>
+
+  <section class="licenses main container">
+    <h2>Лицензии "Социальной стоматологии белогорья"</h2>
+    <div class="row justify-content-center">
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+        <a href="#">
+          <img src="/img/licenses_1.jpg">
+        </a>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+        <a href="#">
+          <img src="/img/licenses_2.jpg">
+        </a>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+        <a href="#">
+          <img src="/img/licenses_3.jpg">
+        </a>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+        <a href="#">
+          <img src="/img/licenses_4.jpg">
+        </a>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+        <a href="#">
+          <img src="/img/licenses_5.jpg">
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="services">
+    <div class="container main">
+      <div class="services_promo">Услуги клиники</div>
+      <div class="row">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+          <a href="#" class="services_item">Совершенно безболезненное лечение зубов любой сложности</a>
+          <p class="services_description">Все манипуляции проводятся с использованием анестетиков последнего поколения
+          </p>
+          <a href="#" class="services_item">Имплантация зубов</a>
+          <p class="services_description">Всемирно известные системы имплантации – Ankylos (Германия) и Osstem (Южная
+            Корея) с технологией быстрого заживления</p>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+          <a href="#" class="services_item">3D-Диагностика</a>
+          <p class="services_description">Объемный снимок, ОПГ (панорамный снимок), прицельный снимок</p>
+          <a href="#" class="services_item">Диагностика и лечение заболеваний пародонта, плазмолифтинг</a>
+          <a href="#" class="services_item">Эстетическая стоматология</a>
+          <p class="services_description">Реставрация, установка виниров, компониров</p>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+          <a href="#" class="services_item">Лечение зубов под наркозом взрослым и детям</a>
+          <a href="#" class="services_item">Отбеливание beyond</a>
+          <a href="#" class="services_item">Лазерное отбеливание зубов</a>
+          <a href="#" class="services_item">Ортодонтическое лечение</a>
+          <p class="services_description">Новейшие брекет-системы, в том числе DAMON</p>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+          <a href="#" class="services_item">Лечение под микроскопом</a>
+          <p class="services_description">Удаление с технологией быстрого заживления), остеопластика (восстановление
+            недостатка костной ткани)</p>
+          <a href="#" class="services_item">Протезирование зубов</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
